995727b78135f111c4de304094b24fcc3cab41fc
AntonSavchuk <savchuk.anton.s@gmail.com>
2025-02-17 12:25:37 +0300

    feat(api): implemented basic API for managing sites

--- availbilityChecker/checker/views.py
+++ availbilityChecker/checker/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Website, SiteStatus
+from .serializers import WebsiteSerializer, SiteStatusSerializer
+
+
+class WebsiteViewSet(viewsets.ModelViewSet):
+    queryset = Website.objects.all()
+    serializer_class = WebsiteSerializer
+
+class SiteStatusViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = SiteStatus.objects.all()
+    serializer_class = SiteStatusSerializer
